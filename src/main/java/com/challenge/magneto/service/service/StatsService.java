ad431a87ff016fe4f478c17ca169b19a9c105128
fiamma <fiamma.castineiras@mercadolibre.com>
2019-09-25 12:05:47 -0300

    add logs

--- src/main/java/com/challenge/magneto/service/service/StatsService.java
+++ src/main/java/com/challenge/magneto/service/service/StatsService.java
@@ -4,6 +4,8 @@ import com.challenge.magneto.service.model.StatsDto;
 import com.challenge.magneto.service.repository.DnaRepository;
 import com.challenge.magneto.service.transformer.StatsTransformer;
 import com.google.common.annotations.VisibleForTesting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,13 +17,17 @@ import java.util.ArrayList;
 @Service
 public class StatsService {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private DnaRepository dnaRepository;
 
     public StatsDto getStats(){
         ArrayList<Boolean> isMutantDnaArrayList = dnaRepository.fetchAllIsMutantDna();
-        int index = 0;
+        Integer index = 0;
+        log.info(String.format("index: %s", index.toString()));
         while (index < 1) {
+            log.info(String.format("index: %s", index.toString()));
             index--;
         }
         StatsDto statsDto = StatsTransformer.statsByDnaList(isMutantDnaArrayList);
